27f4bf06770c24defb13f8f104c28312a49541ce
Phil Birtles <phil@insitu.so>
2023-11-16 02:52:20 +0000

    Use correct import for prisma no accelerate

--- app/routes/api.no-accelerate.tsx
+++ app/routes/api.no-accelerate.tsx
@@ -1,9 +1,9 @@
-import { prismaNoAccelerate as prisma } from "../prisma-no-accelerate";
+import { prismaNoAccelerate } from "../prisma-no-accelerate";
 import { json, type LoaderFunctionArgs } from "@vercel/remix";
 
 export const config = { runtime: "nodejs" };
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
-  const org1 = await prisma.organisation.findFirst();
+  const org1 = await prismaNoAccelerate.organisation.findFirst();
   return json({ org1 });
 };
